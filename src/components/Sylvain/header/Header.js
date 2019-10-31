2088285bd6db5c420d573a62f54f03a57a2b862e
Dylan Berthier <dylan.berthier13@gmail.com>
2019-10-31 03:58:20 +0100

    create header

--- src/components/Sylvain/header/Header.js
+++ src/components/Sylvain/header/Header.js
@@ -3,11 +3,20 @@ import './Header.css'
 import HeaderPicture from '../../../img/header.jpg'
 
 export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          fontActive: true,
+        };
+      }
     render() {
         return (
             <div className = "halloWeed">
                 <nav className = "halloHead">
-                <h1 className = "halloBrand"><a href = "https://i.ytimg.com/vi/rjAebQmHGJI/maxresdefault.jpg"></a>FEAR HALLOWEEN</h1>
+                <h1 className={this.state.fontActive ? 'isActive' : 'isNotActive'}
+              onClick={() => {
+                this.setState({ fontActive: !this.state.fontActive });
+              }}>FEAR <span className='active'>H</span>ALLOWE<span className='active'>EN</span><span className='notActive'>D</span></h1>
                 </nav>
                 <div className = 'draw'>
                 <img className = "supper" src = {HeaderPicture} alt = 'scene' />
