86307ac094cbb60f18e3958bbf85c46ea67fce44
Dylan Berthier <dylan.berthier13@gmail.com>
2019-10-31 04:59:35 +0100

    First Build with header and carousel

--- src/components/Sylvain/header/Header.js
+++ src/components/Sylvain/header/Header.js
@@ -6,7 +6,7 @@ export default class Navbar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          fontActive: true,
+          fontActive: false,
         };
       }
     render() {
